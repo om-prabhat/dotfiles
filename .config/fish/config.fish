017bcf84981608fa3112197e7406a1d97dfa09ef
0x5h4d0w <shadow@0x5.in>
2026-03-25 00:15:34 +0530

    Initial commit for cachyos-niri

---
+++ .config/fish/config.fish
@@ -0,0 +1,49 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+source "$HOME/.cargo/env.fish"
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+export EDITOR=nvim
+function mkcd
+       mkdir -p $argv[1]
+       and cd $argv[1]
+end
+
+#function tmux
+#    command tmux -u $argv
+#end
+
+#function btop
+#    command btop --force-utf $argv
+#end
+
+
+function tenv
+	export $argv[1]=$argv[2]
+	tmux set-env -g $argv[1] "$argv[2]"
+end
+
+# sudo path fix
+alias sudo="sudo env PATH=\"$PATH\" $argv"
+
+alias mic="wpctl set-volume @DEFAULT_AUDIO_SOURCE@ $argv"
+
+# bloodhound
+alias bloodhound="docker compose -f /home/shadow/.config/bloodhound/docker-compose.yml up -d"
+alias bloodhound-stop="docker compose -f /home/shadow/.config/bloodhound/docker-compose.yml stop"
+alias bloodhound-python="bloodhound-ce-python"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# terminal-wakatime setup
+set -gx PATH "$HOME/.wakatime" $PATH
+terminal-wakatime init fish | source
